a0181d980c17a63c5ceb07c50d7bdf5beef5aa7a
George Neonakis <gnan88@mun.ca>
2018-11-24 14:33:40 -0330

    Added scramble and reverse mutation functions

--- Assets/TSP/TSP_main.py
+++ Assets/TSP/TSP_main.py
@@ -8,6 +8,7 @@ from route import Route
 from parent_selection import tournament_selection
 from survivor_selection import survivor_selection
 from mutation import swap_mutation
+from mutation import reverse_mutation
 from profiler import profile
 
 pop_size = 100  #must be a multiple of 4
@@ -15,7 +16,7 @@ mating_pool_size = int(pop_size * 0.5)
 tournament_size = 4
 mut_rate = 0.5
 xover_rate = 0.8
-gen_limit = 1000
+gen_limit = 100
 staling_limit = 10
 
 @profile
@@ -24,7 +25,7 @@ def main():
     staling = 0
     prevAverage = 0
     ## Create initial population and calculate initial fitness
-    population = permutation(pop_size, create_cities(file_manager.SAHARA_PATH))
+    population = permutation(pop_size, create_cities(file_manager.CANADA_PATH))
     while current_gen < gen_limit and staling < staling_limit:
         parents = tournament_selection(population, mating_pool_size, tournament_size)
         r.shuffle(parents)
@@ -46,9 +47,9 @@ def main():
                 off1 = Route(parents[i+1].get_cities().copy(), False)
             ## mutation
             if r.random() < mut_rate:
-                off0 = swap_mutation(off0)
+                off0 = reverse_mutation(off0)
             if r.random() < mut_rate:
-                off1 = swap_mutation(off1)
+                off1 = reverse_mutation(off1)
             offspring.append(off0)
             offspring.append(off1)
             i += 2
--- Assets/TSP/mutation.py
+++ Assets/TSP/mutation.py
@@ -1,4 +1,5 @@
 from random import randint
+from random import shuffle
 from route import Route
 
 def swap_mutation(individual):
@@ -18,3 +19,38 @@ def swap_mutation(individual):
     mutant[first], mutant[second] = mutant[second], mutant[first]
     return Route(mutant, False)
 
+def reverse_mutation(individual):
+    """
+       Reverses a segment of a permutation.
+       args:
+           list : a route individual
+       returns:
+           list : the same individual with a segment reversed
+    """
+
+    mutant = individual.get_cities()
+    first = randint(0, len(mutant)-1)
+    second = randint(0, len(mutant))
+    while (second == first):
+        second = randint(0, len(mutant))
+    mutant[first:second] = reversed(mutant[first:second])
+    return Route(mutant, False)
+
+def scramble_mutation(individual):
+    """
+       Scrambles a segment of a permutation.
+       args:
+           list : a route individual
+       returns:
+           list : the same individual with a segment scrambled
+    """
+
+    mutant = individual.get_cities()
+    first = randint(0, len(mutant)-1)
+    second = randint(0, len(mutant))
+    while (second == first):
+        second = randint(0, len(mutant))
+    copy = mutant[first:second]
+    shuffle(copy)
+    mutant[first:second] = copy
+    return Route(mutant, False)
