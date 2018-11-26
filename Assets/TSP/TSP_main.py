e5a5d17a797c4f16af84c69ce3206f069e9ceefd
George Neonakis <gnan88@mun.ca>
2018-11-26 20:04:23 -0330

    Implemented dynamic mutation / separated matplotlab from profiler

--- Assets/TSP/TSP_main.py
+++ Assets/TSP/TSP_main.py
@@ -21,18 +21,24 @@ mut_rate = 0.1
 xover_rate = 0.9
 gen_limit = 10000
 staling_limit = 10
+summaryAvgList = []
+summaryBestList = []
 
 @profile
 def main():
     current_gen = 0
     staling = 0
     prevAverage = 1
-    summaryAvgList = []
-    summaryBestList = []
     ## Create initial population and calculate initial fitness
-    population = permutation(pop_size, create_cities(file_manager.SAHARA_PATH))
+    population = permutation(pop_size, create_cities(file_manager.URUGUAY_PATH))
+    useHeuristic = False
+    useSwap = False
+    staled = False
+    heuristicThreshold = len(population)
+    swapThreshold = len(population) * 5
     stalingThreshold = len(population) * 10
-    while current_gen < gen_limit and staling < staling_limit:
+    currentThreshold = heuristicThreshold
+    while current_gen < gen_limit and not staled:
         parents = tournament_selection(population, mating_pool_size, tournament_size)
         r.shuffle(parents)
         offspring = []
@@ -48,9 +54,19 @@ def main():
                 off1 = Route(parents[i+1].get_cities().copy(), False)
             ## mutation
             if r.random() < mut_rate:
-                off0 = heuristic_swap(off0)
+                if (useSwap):
+                    off0 = swap_mutation(off0)
+                elif (useHeuristic):
+                    off0 = heuristic_swap(off0)
+                else:
+                    off0 = scramble_mutation(off0)
             if r.random() < mut_rate:
-                off1 = heuristic_swap(off1)
+                if (useSwap):
+                    off1 = swap_mutation(off1)
+                elif (useHeuristic):
+                    off1 = heuristic_swap(off1)
+                else:
+                    off1 = scramble_mutation(off1)
             offspring.append(off0)
             offspring.append(off1)
             i += 2
@@ -66,19 +82,31 @@ def main():
         print("Best fitness: ", bestFit)
         print("Average fitness: ", average)
 
-        if (average/prevAverage > (stalingThreshold-1)/stalingThreshold and average/prevAverage < (stalingThreshold+1/stalingThreshold)):
+        if (average/prevAverage > (currentThreshold-1)/currentThreshold and average/prevAverage < (currentThreshold+1/currentThreshold)):
             staling += 1
         else:
+            staling = 0
+        if (staling == staling_limit):
+            if not useHeuristic:
+                useHeuristic = True
+                currentThreshold = swapThreshold
+                print("Changed from scramble to heuristic swap!")
+            elif not useSwap:
+                useSwap = True
+                currentThreshold = stalingThreshold
+                print("Changed from heuristic to random swap!")
+            else:
+                staled = True
             staling = 0
         prevAverage = average
         current_gen += 1
     # print the final best solution(s)
-    if (staling == staling_limit):
+    if (staled):
         print("Population staled!")
     else:
         print("Gen limit reached!")
-    summary.visualize_avg_best_fit(summaryAvgList, summaryBestList)
 
 
 if __name__ == '__main__':
     main()
+    summary.visualize_avg_best_fit(summaryAvgList, summaryBestList)
