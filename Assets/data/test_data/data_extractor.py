52701b03c035b93fcc945777d7ab1cbc5fa11231
Scott Jennings <33096273+sdj110@users.noreply.github.com>
2018-12-10 14:50:37 -0330

    Updated data extractor to search for jsons in subdir and extract all data.

--- Assets/data/test_data/data_extractor.py
+++ Assets/data/test_data/data_extractor.py
@@ -1,8 +1,8 @@
 
-
+from pathlib import Path
+import os
 import sys
 import json
-
 
 def read_file(filename):
     with open(filename, 'r') as f:
@@ -15,35 +15,41 @@ def write_file(filename, data_dict):
             f.write(key + '\n')
             f.write(data_dict[key])
             f.write('\n\n')
-def format_string(data):
-    pass
 
 if __name__ == '__main__':
 
-    filename = "sahara/500p_2t.json"
-    if len(sys.argv) == 2:
-        filename = sys.argv[1]
+    pathlist = Path(os.getcwd()).glob('**/*.json')
+    for path in pathlist:
+        # because path is object not string
+        path_in_str = str(path)
+        
+        filename = path_in_str
 
-    ## Fix poor formatting in data file.
-    data = read_file(filename)
-    format_data = data.replace('}{', '}!{').split('!')
+        ## Fix poor formatting in data file.
+        data = read_file(filename)
+        format_data = data.replace('}{', '}!{').split('!')
 
-    ## build dictionary of dicts thanks to json lib
-    dict_list = []
-    for d in format_data:
-        dict_list.append(json.loads(d))
+        ## build dictionary of dicts thanks to json lib
+        dict_list = []
+        for d in format_data:
+            dict_list.append(json.loads(d))
 
-    ## extract needed into into seperate lists
-    data_dict = {
-        "time" : "",
-        "generation" : "",
-        "best_fitness" : "",
-        "avg_fitness" : ""
-    }
-    for d in dict_list:
-        data_dict["time"] += str(d["elapsed_time"].split(":")[2]) + ","
-        data_dict["generation"] += str(d["generation"]) + ","
-        data_dict["best_fitness"] += str(d["best_fitness"]) + ","
-        data_dict["avg_fitness"] += str(d["avg_fitness"]) + ","
+        ## extract needed data into seperate strings
+        data_dict = {
+            "time" : "",
+            "generation" : "",
+            "best_fitness" : "",
+            "avg_fitness" : ""
+        }
+        for d in dict_list:
+            data_dict["time"] += str(d["elapsed_time"].split(":")[2]) + ","
+            data_dict["generation"] += str(d["generation"]) + ","
+            data_dict["best_fitness"] += str(d["best_fitness"]) + ","
+            data_dict["avg_fitness"] += str(d["avg_fitness"]) + ","
 
-    write_file(filename + "_results.txt", data_dict)
+        write_file(filename + "_results.txt", data_dict)
+
+        print("Data saved to file ", filename + "_results.txt")
+
+
+    
